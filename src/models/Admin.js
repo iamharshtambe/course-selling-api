8c9fcf0b79afa83b315a2b9df5cbb7db329d5fbd
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-26 21:31:35 +0530

    added Admin model

---
+++ src/models/Admin.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const adminSchema = new mongoose.Schema(
+   {
+      firstName: {
+         type: String,
+         required: true,
+         minLength: 1,
+         maxLength: 20,
+         index: true,
+      },
+      lastName: {
+         type: String,
+         required: true,
+         minLength: 1,
+         maxLength: 20,
+         index: true,
+      },
+      email: {
+         type: String,
+         required: true,
+         unique: true,
+         lowercase: true,
+         trim: true,
+         validate: {
+            validator: validator.isEmail,
+            message: 'Invalid email',
+         },
+      },
+      password: {
+         type: String,
+         required: true,
+         validate: {
+            validator: validator.isStrongPassword,
+            message: 'Password must be strong',
+         },
+      },
+   },
+   { timestamps: true }
+);
+
+export const Admin = mongoose.model('Admin', adminSchema);
