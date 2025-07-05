c446b65de49b9189e3a732fe72f2379c611e6229
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-05 23:14:12 +0530

    restart repo

--- src/routes/coursesRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-export const coursesRouter = express.Router();
-
-coursesRouter.get('/', (req, res) => {});
-
-coursesRouter.get('/purchased', (req, res) => {});
-
-coursesRouter.post('/:courseId/purchase', (req, res) => {});
