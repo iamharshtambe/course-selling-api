a7dd8c89dc5e480cb7ec9d3ccecdd4c6da76de22
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-07 21:39:07 +0530

    add primitives

--- src/01-primitives.ts
+++ src/01-primitives.ts
@@ -1,9 +1,13 @@
+// Who am I? Let's define my name as a string
 const myName: string = 'Harsh';
 
+// Age is just a number — literally!
 const myAge: number = 22;
 
+// Love life status: strictly boolean 🚫💍
 const isMarried: boolean = false;
 
+// Time to spill the tea ☕ (aka print the details)
 console.log(
    `My name is ${myName}. I am ${myAge} years old and my marital status is ${isMarried}`
 );
