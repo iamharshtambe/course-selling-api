db3377c71b18a393fd99384d7f0f9a4dff46856a
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-07 21:52:51 +0530

    add enums

---
+++ src/04-enums.ts
@@ -0,0 +1,10 @@
+// 📦 A box of status codes — giving names to meaningful numbers
+enum StatusCodes {
+   NotFound = 404, // Page vanished into the void 🚫
+   Success = 200, // Everything's awesome ✅
+   Accepted = 202, // Got it, working on it ⏳
+   BadRequest = 400, // Something's fishy with the request 🐟
+}
+
+// Logging a sweet success 🍀
+console.log(StatusCodes.Success);
