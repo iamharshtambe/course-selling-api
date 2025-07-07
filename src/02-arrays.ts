b82d4a8995c710a11ab5982a556219d409eee016
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-07 21:39:22 +0530

    add arrays

---
+++ src/02-arrays.ts
@@ -0,0 +1,13 @@
+// A squad of strings — these are my awesome friends 👯‍♂️
+const myFriends: string[] = ['Sumit', 'Sejal', 'Ishu'];
+
+// My lucky numbers — all nicely typed as numbers 🔢✨
+const myFavouriteNumbers: number[] = [24, 25, 31, 13];
+
+// True or false — the most dramatic kind of array 😅
+const booleanArray: boolean[] = [true, false];
+
+// Let's show off my data 📢
+console.log(myFriends);
+console.log(myFavouriteNumbers);
+console.log(booleanArray);
