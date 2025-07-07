585b296bac66c38804d4653a7e39f7396ae0635d
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-07 21:29:07 +0530

    add primitives

---
+++ src/01-primitives.ts
@@ -0,0 +1,9 @@
+const myName: string = 'Harsh';
+
+const myAge: number = 22;
+
+const isMarried: boolean = false;
+
+console.log(
+   `My name is ${myName}. I am ${myAge} years old and my marital status is ${isMarried}`
+);
