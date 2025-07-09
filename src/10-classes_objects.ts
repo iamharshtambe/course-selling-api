0917c29f7fa6d9bc2f3753766845f4122f1992c5
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-09 20:06:52 +0530

    create getters and setters

--- src/10-classes_objects.ts
+++ src/10-classes_objects.ts
@@ -1,22 +1,38 @@
 // 🚂 Train class — a blueprint for all kinds of trains
 class Train {
    constructor(
+      readonly trainId: string, // Train ID
       public type: string, // Type of train: express, passenger, goods
-      public name: string, // Name of the train (e.g., Shatabdi)
+      public _name: string, // Name of the train (e.g., Shatabdi)
       public year: number, // Year the train was launched
       public coaches: number, // Number of coaches it has
-      public isRunning: boolean // Is the train currently running?
+      public isRunning?: boolean // Is the train currently running?
    ) {}
 
    // 📣 Method to announce train departure
    trainDepart() {
       console.log(`${this.name} has ${this.coaches} coaches`);
    }
+
+   printTrainId() {
+      console.log(this.trainId);
+   }
+
+   get name() {
+      return this._name;
+   }
+
+   set name(value: string) {
+      this.name = value;
+   }
 }
 
 // 🛤️ Creating an instance of Train
-const t1 = new Train('express', 'shatabdi', 1990, 16, true);
+const t1 = new Train('#12112', 'express', 'shatabdi', 1990, 16);
 
 // 📦 Logging train info and triggering the departure method
 console.log(t1);
+
 t1.trainDepart();
+
+console.log(t1.name);
